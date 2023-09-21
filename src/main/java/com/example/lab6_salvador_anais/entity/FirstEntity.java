b1d73865cd609034d5306707030de09b91ed0f4a
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2023-09-21 10:27:07 -0500

    adicionar

---
+++ src/main/java/com/example/lab6_salvador_anais/entity/FirstEntity.java
@@ -0,0 +1,4 @@
+package com.example.lab6_salvador_anais.entity;
+
+public class FirstEntity {
+}
---
+++ src/main/java/com/example/lab6_salvador_anais/repository/FirstRepository.java
@@ -0,0 +1,4 @@
+package com.example.lab6_salvador_anais.repository;
+
+public interface FirstRepository {
+}
