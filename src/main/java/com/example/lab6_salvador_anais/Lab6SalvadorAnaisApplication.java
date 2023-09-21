87f1c9b969080a48e19efa0286684dbd4947dd7e
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2023-09-21 10:15:22 -0500

    first commit

---
+++ src/main/java/com/example/lab6_salvador_anais/Lab6SalvadorAnaisApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab6_salvador_anais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6SalvadorAnaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab6SalvadorAnaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab6_salvador_anais/Lab6SalvadorAnaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_salvador_anais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6SalvadorAnaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
